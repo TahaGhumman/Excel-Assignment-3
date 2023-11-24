0d399226d9be33e38ad3b88a3b7d71379e993cee
Taha Ghumman <mtghumma@ualberta.ca>
2023-11-23 23:38:35 -0700

    Final + Documentation Done

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
+"""
+Taha Ghumman, Troy Williams, Rashed Amani
+LEC 1D02
+Nov 24th, 2023
+--
+
+This program aims to cleanse the data necessary to report the amount of collisions based off different categories.
+The program focuses on finding which animal collided the most, which year the most incidents happened, which month the most incidents happend, and which airline/operator caused the most incidents.
+This is done by finding the needed columns of each focus, then counting the occurences of the value of each cell in the column.
+The program takes in different aircraft data related to incident reports as an excel/.xlsx file to read through it. 
+The program relies on the OpenPyXL library to help read the necessary data.
+
+References:
+Python Openpyxl, JavaTpoint, 2021. Python Openpyxl Tutorial - javatpoint
+Hands-on Python Openpyxl Tutorial With Examples (softwaretestinghelp.com)
+
+Python Excel, 2021. Openpyxl Tutorial - Read, Write & Manipulate xlsx files in Python -
+Python Excel
+
+Contributions:
+
+The data cleansing, finding a species common name, counting the data and main function were created by Taha Ghumman.
+
+The creation of the excel file, writing to the excel file, alphabetical sorting and graph creation were done by Troy Williams.
+
+The value deduction of the top values and maximum values of each column were made by Rashed Amani.
+
+"""
+
 import openpyxl as pyxl
 from openpyxl import Workbook
 from openpyxl.chart import BarChart, Reference
 import re
 
-workBookPath = 'mediumAircraftData.xlsx'
-newDataSheetPath = r'wildlifeAnalysis.xlsx'
+workBookPath = "mediumAircraftData.xlsx"
+newDataSheetPath = r"wildlifeAnalysis.xlsx"
 print("Loading workbook...")
 
 workBook = pyxl.load_workbook(workBookPath)
@@ -14,7 +43,17 @@ workSheet = workBook.active
 
 wildlifeAnalysis = Workbook()
 
+# Getting Species Name - Taha Ghumman
 def getSpeciesName(animalName: str):
+    """
+    Given the "animalName" specified, it will strip the full name of the animal into its common name.
+    If there is multiple animals it will make sure to separate each animal and account for them as individual animals.
+    The function will also make sure that if the animal name ends with an "s" or is a plural of an animal - such as geese - to fix the plurality.
+    The function does this by using regex to allow it to remove all unneccessary items that do not need to be parsed.
+
+    :params animalName: The name of the animal being stripped of its full name down to the common name.
+    :return splitAnimalName/animals: returns only the common name that it has found by stripping away the common name.
+    """
     splitAnimalName = animalName.split(",")
 
     if len(splitAnimalName) == 1:
@@ -39,7 +78,16 @@ def getSpeciesName(animalName: str):
                 animals.append(animalNames[-1])
         return animals
 
+# Counting All Animals - Taha Ghumman
 def countAnimals(animalsWorksheet: dict):
+    """
+    Given the animal column, it will run the "getSpeciesName" function to strip the animal of its full name down to the common name.
+    The code also makes sure to account for the blanks by filling them with UNKNOWN.
+    The code will then loop through each cell in the column and tally up all the animals. If it encounters a new animal, it will make a new key in the animalDict.
+
+    :params animalsWorksheet: The given column containing all the cells with animals.
+    :return animalDict: returns the new counted animal column with how many incidents there are per animal. 
+    """
     animalDict = {}
     for animalData in range(len(animalsWorksheet)): 
         fullName = animalsWorksheet[animalData].value
@@ -62,7 +110,14 @@ def countAnimals(animalsWorksheet: dict):
 
     return animalDict
 
+# Counting All Items - Taha Ghumman
 def countItems(someWorksheet: dict):
+    """
+    Counts the items given in a specified column. It will make sure that if a new data point is given, it will make it a new key.
+    If the data point already exists, it will add onto the current value by one.
+
+    :return countedDict: Returns a dictionary with the counted number for each key occurence in someWorksheet.
+    """
     countedDict = {}
     for worksheetData in range(len(someWorksheet)):
         someValue = someWorksheet[worksheetData].value
@@ -72,8 +127,18 @@ def countItems(someWorksheet: dict):
             countedDict[someValue] = 1
     return countedDict
 
+# Maximum Values and Top Values - Rashed Amani
 def returnHighestandTop(someDict: dict, threshold: int = 0):
+    """
+    Returns the Highest values within of "someDict" as well as the top values within a given percentage threshold.
+    The function accounts for ALL highest values even if there are many with the same maximum value. It does so by finding the key with the max value and then comparing with other keys and their values.
+    The function finds the top values within a given percentage by seeing if they pass a threshold percentage. If they do, they are all considered to be within the percentage required to be appended to the list.
 
+    :params someDict: The dictionary used to find its highest data points and the top data within a threshold.
+            threshold: The threshold as a percentage used to see if values within the list are higher than that amount based off the highest given value. If no threshold is specified, it will assume you want all the values in the dictionary.
+    :return highestDataPoints: The keys of the maximum values within of the given dictionary.
+            topData: A list of tuples with its first value representing the key and the second value representing the value. The indices in the list all must be greater than the threshold percentage based on the maximum value.
+    """
     highestData = max(someDict, key=someDict.get)
     highestDataPoints = []
     topData = []
@@ -89,8 +154,9 @@ def returnHighestandTop(someDict: dict, threshold: int = 0):
     
     return highestDataPoints, topData
 
+# Creating Excel Sheet and Graphing - Troy Williams
 def writeToExcel(dataList: list, name: str, itemName: str, chartTitle: str) :
-    '''
+    """
     Function that takes in a multi-dimensional list of data and sorts it alphabetically or ascending, (dependent on integer or string values.)
     The sorted list is then iterated through and placed in to a new excel sheet.
     Once the data placement is finished, the table of values is used to make a bar graph.
@@ -98,7 +164,7 @@ def writeToExcel(dataList: list, name: str, itemName: str, chartTitle: str) :
             name: name used for the new sheet being created for the data.
             itemName: name used for the unique items, header for column one, and title for the x-axis of the chart.
             chartTitle: specific name used for the title of the chart.
-    '''
+    """
     wildlifeAnalysis.create_sheet(name)
     sheet = wildlifeAnalysis[name]
     sortedList = sorted(dataList)
@@ -122,10 +188,18 @@ def writeToExcel(dataList: list, name: str, itemName: str, chartTitle: str) :
     chart.x_axis.title = itemName
     chart.y_axis.title = "COLLISIONS"
     sheet.add_chart(chart,"D1")
-    
-    return
-    
+
+# Main Function // Cleaning the Data - Taha Ghumman
 def cleanData():
+    """
+    Main function body in which we grab each usable column from our main worksheet and name them as "xWorksheet", where x represents the data of the column.
+    The function then grabs a dictionary where all the values are counted using the "countItems" and "countAnimals" function, for the number of occurences of their keys.
+    The function is then grabbing the maximum values within the specified dictionaries for each column using "returnHighestandTop" as well as grabbing the top values in a given threshold. If there is no threshold specified; it will return all the values.
+    The function then checks to see if there is more than 15 airlines, if there is, it will only get the ones within the 10% threshold of the highest value in the airlines dictionary.
+    The function then prints each individual statistic with their highest value as well as the total incidents within that statistic.
+    The function will then write each value it has grabbed from the calculated top values into an excel sheet and graph them into a bar graph based using the function "WriteToExcel".
+    The function fixes the excel sheet by making a correction by removing the extra sheet the code starts with and saves all the sheet data into a directed data sheet path.
+    """
     animalsWorksheet, yearWorksheet, monthWorksheet, airlineWorksheet = workSheet["AF"][1:], workSheet["B"][1:], workSheet["C"][1:], workSheet["F"][1:]
 
     animalDict, yearDict, monthDict, airlineDict = countAnimals(animalsWorksheet), countItems(yearWorksheet), countItems(monthWorksheet), countItems(airlineWorksheet)
@@ -138,18 +212,18 @@ def cleanData():
     if len(airlineDict) > 15:
         highestAirline, topAirlines = returnHighestandTop(airlineDict, 10)
 
-    print(f"The animals(s) mostly involved in incidents are: {highestAnimals} with {animalDict[highestAnimals[0]]} incidents total.")
+    print(f"The animal(s) mostly involved in incidents are: {highestAnimals} with {animalDict[highestAnimals[0]]} incidents total.")
     print(f"The year(s) with most accidents are: {highestYear}, with {yearDict[highestYear[0]]} incidents total")
     print(f"The month(s) with most accidents are: {highestMonth}, with {monthDict[highestMonth[0]]} incidents total")
     print(f"The airline(s) mostly involved in accidents are: {highestAirline}, with {airlineDict[highestAirline[0]]} incidents total")
 
 
-    writeToExcel(topTenPercentAnimals, 'ChartForAnimals', 'ANIMALS', 'Animals Involved in Aircraft Collisions')
-    writeToExcel(topAirlines, 'ChartForAirlines', 'AIRLINES', 'Airlines Involved in Aircraft Collisions')
-    writeToExcel(allYears, 'ChartForYears', 'YEARS', 'Aircraft Collisions by Year')
-    writeToExcel(allMonths, 'ChartForMonths', 'MONTHS', 'Aircraft Collisions by Month')
+    writeToExcel(topTenPercentAnimals, "ChartForAnimals", "ANIMALS", "Animals Involved in Aircraft Collisions")
+    writeToExcel(topAirlines, "ChartForAirlines", "AIRLINES", "Airlines Involved in Aircraft Collisions")
+    writeToExcel(allYears, "ChartForYears", "YEARS", "Aircraft Collisions by Year")
+    writeToExcel(allMonths, "ChartForMonths", "MONTHS", "Aircraft Collisions by Month")
 
-    del wildlifeAnalysis['Sheet']
+    del wildlifeAnalysis["Sheet"]
     wildlifeAnalysis.save(newDataSheetPath)
     
 cleanData()
